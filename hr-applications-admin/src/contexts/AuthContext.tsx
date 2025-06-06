48e94a11e1c45149a7dedb5c7e62ede21e5ccd8c
stephen kingsley femi <107176184+Dsking32@users.noreply.github.com>
2025-06-06 20:47:16 +0100

    Update AuthContext.tsx

--- hr-applications-admin/src/contexts/AuthContext.tsx
+++ hr-applications-admin/src/contexts/AuthContext.tsx
@@ -37,7 +37,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   }, []);
 
   const login = async (email: string, password: string) => {
-    const res = await fetch('http://localhost:5000/api/auth/signin', {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/signin`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -60,7 +60,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   };
 
   const signup = async (name: string, email: string, password: string) => {
-    const res = await fetch('http://localhost:5000/api/auth/signup', {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/signup`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
